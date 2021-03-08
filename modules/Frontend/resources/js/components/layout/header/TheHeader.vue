79851b7d2ca100435322f7ac663bbbcc9aad95ba
Yurii Kinakh <extrem7@meta.ua>
2021-03-08 11:33:30 +0200

    Logo user driven route

--- modules/Frontend/resources/js/components/layout/header/TheHeader.vue
+++ modules/Frontend/resources/js/components/layout/header/TheHeader.vue
@@ -1,12 +1,14 @@
 <template>
     <header class="header header-account">
         <div class="header-wrapper container">
-            <a :href="route('home')" class="d-block">
+            <a :href="link"
+               class="d-block">
                 <img alt="logo" class="header-logo" src="/dist/img/logo.svg">
             </a>
             <nav class="header-account-navigation">
-                <button class="btn btn-show-search" @click="$bus.emit('search-toggle')">
-                    <svg-vue icon="search-thin"></svg-vue>
+                <button class="btn btn-show-search"
+                        @click="$bus.emit('search-toggle')">
+                    <SvgVue icon="search-thin"/>
                 </button>
                 <slot></slot>
             </nav>
@@ -20,6 +22,17 @@ export default {
         async logout() {
             await this.axios.post(this.route('logout'))
             location.href = this.route('home')
+        }
+    },
+    computed: {
+        user() {
+            return this.$store.state.user
+        },
+        link() {
+            if (this.user) {
+                return this.route(this.user.is_volunteer ? 'vacancies.search' : 'company.board')
+            }
+            return this.route('home')
         }
     }
 }
