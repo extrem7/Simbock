7f176ea7f832695af2b06d2639ccd855c38f6e4a
urix <extremtmnt@gmail.com>
2021-07-30 09:04:10 +0300

    feat: googletagmanager

--- modules/Frontend/resources/views/layouts/master.blade.php
+++ modules/Frontend/resources/views/layouts/master.blade.php
@@ -20,8 +20,27 @@
     <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#7b68ee">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#7B68EE">
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || []
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            })
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''
+            j.async = true
+            j.src =
+                'googletagmanager.com/gtm.js?id=' + i + dl
+            f.parentNode.insertBefore(j, f)
+        })(window, document, 'script', 'dataLayer', 'GTM-5SPK2PM')</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="{{ $bodyClass }}">
+<noscript>
+    <iframe src="googletagmanager.com/ns.html?id=GTM-5SPK2PM"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <div id="app">
     <app>
         @yield('content')
